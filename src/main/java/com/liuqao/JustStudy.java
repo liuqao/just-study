a655046e8aa407a1893c17d29baec2a3c0180e19
liuqao <18996551524@189.cn>
2020-06-19 10:26:55 +0800

    项目初始化

---
+++ src/main/java/com/liuqao/JustStudy.java
@@ -0,0 +1,11 @@
+package com.liuqao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustStudy {
+    public static void main(String[] args) {
+        SpringApplication.run(JustStudy.class, args);
+    }
+}
